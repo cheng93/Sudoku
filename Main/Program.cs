38ee8a063b3e5ca17fa74184fd90566440760107
cheng93 <derek.c@hotmail.co.uk>
2015-07-08 20:46:46 +0100

    Main.dll

---
+++ Main/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Sudoku;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var board = new Board("900240000050690231020050090090700320002935607070002900069020073510079062207086009");
+            board.Solve();
+            Console.ReadLine();
+        }
+    }
+}

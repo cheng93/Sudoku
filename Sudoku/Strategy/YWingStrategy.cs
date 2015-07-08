520de9d00ef7b4c0a6fd156750e1a3847684be5b
cheng93 <derek.c@hotmail.co.uk>
2015-07-08 20:47:24 +0100

    Implementation of Y Wing strategy

---
+++ Sudoku/Strategy/YWingStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sudoku.Strategy
+{
+    public class YWingStrategy : BaseSudokuStrategy
+    {
+        public YWingStrategy(Board board)
+            : base(board)
+        {
+        }
+
+        public override bool Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
